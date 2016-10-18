9132d70c2be5961e2f96359e2658b57a4e291a2a
Taylor Redding <taylorhredding@gmail.com>
2016-10-17 17:01:01 -0700

    First Commit

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -8,16 +8,30 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lblUpdate.text = ""
     }
+    @IBOutlet weak var txtField: UITextField!
 
+    @IBOutlet weak var lblUpdate: UILabel!
+    
+    var strHolder = ""
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func btnClick(_ sender: AnyObject) {
+        
+        strHolder = txtField.text!
+        lblUpdate.text = strHolder
+        txtField.text = ""
+        
     }
 
 
